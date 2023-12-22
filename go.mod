39239a7065cc6efb74ba4be1297a69a05c07cfc9
Christopher Schoeder <christopher.schoeder@protonmail.com>
2023-12-22 09:42:34 -0600

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/freightcms/carrier-models
+
+go 1.21.1
---
+++ src/main.go
@@ -0,0 +1,49 @@
+package main
+
+// carrier identification codes enum
+// see https://kb.freightcms.com/carriers/ for more information
+const (
+	IATA = "IATA"
+	ICC = "ICC"
+	IMO = "IMO"
+	MC = "MC"
+	SCAC = "SCAC"
+	USDOT = "USDOT"
+)
+
+type CarrierIdnetifycationCode struct {
+	id string
+	code string
+}
+
+type CarrierIntlAddress struct {
+	id string
+	address1 string
+	address2 string
+	address3 string
+	region string
+	stateOrProvince string
+	zipOrPostalCode string
+	country string
+}
+
+type CarrierContact struct {
+	id string
+	name string
+	email string
+	phone string
+	fax string
+	preferredContactMethod string
+	reference string
+}
+
+type FreightCarrier struct {
+	id string
+	name string
+	dba string
+	website string
+	contact CarrierContact
+	mailingAddress CarrierIntlAddress
+	billingAddress CarrierIntlAddress
+	identificationCode []CarrierIdnetifycationCode
+}
