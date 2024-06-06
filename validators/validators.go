b45098546789eab108339ebfbe7b80e7af1aec0b
squishedfox <christopher.schoeder@protnmail.com>
2024-06-06 18:49:34 -0500

    Modify the interface for being able to create a validation error func type

--- validators/validators.go
+++ validators/validators.go
@@ -2,11 +2,10 @@ package validators
 
 import "context"
 
-type ValidationErrors interface {
-	// JSON Creates a new JSON interface which can then be appended to the responses of certain objects or be output tot he user.
-	JSON(ctx context.Context) interface{}
-}
-
 // Validator is the interface that can be implemented for schemas or models during initial call for creating, deleting, or updating.
 // Validators can expand a wide range of use cases, form user permissions with passed database context or user principles
-type ValidatorFunc func(ctx context.Context, val any) []ValidationErrors
+type ValidatorFunc func(ctx context.Context, val any) []error
+
+func CreateValidatorFunc(callable func(ctx context.Context, val any) []error) ValidatorFunc {
+	return ValidatorFunc(callable)
+}
