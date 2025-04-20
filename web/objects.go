b8da8d126c408499dcadee21d0b21ded571c8b33
squishedfox <christopher.schoeder@protonmail.com>
2025-04-19 19:51:33 -0500

    wrap up making carrier schema

--- web/objects.go
+++ web/objects.go
@@ -1,35 +1,35 @@
 package web
 
 import (
-	orgWeb "github.com/freightcms/organizations/web"
 	"github.com/graphql-go/graphql"
 )
 
-var InsuranceObject *graphql.Object = &graphql.NewObject(graphql.ObjectConfig{
-    Name: "Insurance",
-    Fields: graphql.Fields{
-        "policyProvider": &graphql.Field{
-            Description: "Whom is the financial backing provider or broker for the insurance",
-            Type: &graphql.NewNonNull(graphql.String),
-        },
-        "policyNumber": &graphql.Field{
-            Type: &graphql.NewNonNull(graphql.String),
-        },
-        "effectiveDate", &graphql.Field{
-            Type: &graphql.NewNonNull(graphql.DateTime)
-        },
-        "expirationDate": &graphql.Field{
-            Type: &graphql.NewNonNull(graphql.DateTime)
-        },
-        "insuranceType": &graphql.Field{
-            Type: &graphql.NewNonNull(graphql.String),
-        },
-        "insuredAmount": &graphql.Field{
-            Type: &graphql.NewNonNull(graphql.Float),
-        }
-    },
-})
-var IDObject *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
+var (
+	InsuranceObject *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
+		Name: "Insurance",
+		Fields: graphql.Fields{
+			"policyProvider": &graphql.Field{
+				Description: "Whom is the financial backing provider or broker for the insurance",
+				Type:        graphql.NewNonNull(graphql.String),
+			},
+			"policyNumber": &graphql.Field{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+			"effectiveDate": &graphql.Field{
+				Type: graphql.NewNonNull(graphql.DateTime),
+			},
+			"expirationDate": &graphql.Field{
+				Type: graphql.NewNonNull(graphql.DateTime),
+			},
+			"insuranceType": &graphql.Field{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+			"insuredAmount": &graphql.Field{
+				Type: graphql.NewNonNull(graphql.Float),
+			},
+		},
+	})
+	IDObject *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
 		Name: "ID",
 		Fields: graphql.Fields{
 			"id": &graphql.Field{
@@ -37,17 +37,33 @@ var IDObject *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
 			},
 		},
 	})
-var CarrierObject *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
+	CarrierIdentifierObject = graphql.NewObject(graphql.ObjectConfig{
+		Name: "Carrier Identifiers",
+		Fields: graphql.Fields{
+			"type": &graphql.Field{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+			"value": &graphql.Field{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+		},
+	})
+	CarrierObject *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
 		Name: "Carrier",
 		Fields: graphql.Fields{
 			"id": &graphql.Field{
 				Type: graphql.NewNonNull(graphql.String),
 			},
-            "isActive": &graphql.Field{
-                Description: "Flag for indicating if the carrier is active in the market",
-                Type: graphql.NewNonNull(graphql.Boolean),
-            },
-            "insurance" &graphql.NewList(InsuranceObject)
-        },
+			"isActive": &graphql.Field{
+				Description: "Flag for indicating if the carrier is active in the market",
+				Type:        graphql.NewNonNull(graphql.Boolean),
+			},
+			"insurance": &graphql.Field{
+				Type: graphql.NewList(InsuranceObject),
+			},
+			"identifeirs": &graphql.Field{
+				Type: graphql.NewList(CarrierIdentifierObject),
+			},
+		},
 	})
 )
